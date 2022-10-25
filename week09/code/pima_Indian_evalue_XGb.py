2384c25012b01d524ff052deff05ad31f3f4e6ed
minari-c <h990727@gmail.com>
2022-10-25 19:28:35 +0900

    "week09 third"

--- week09/code/pima_Indian_evalue_XGb.py
+++ week09/code/pima_Indian_evalue_XGb.py
@@ -1,31 +1,17 @@
-import numpy as np
 import pandas as pd
-from sklearn.svm import SVR
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import cross_val_score
+from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV, cross_val_score
 
-# 피마 인디언 당뇨병 데이터셋을 불러옵니다.
-df = pd.read_csv('../csv/pima-indians-diabetes3.csv')
+df = pd.read_csv('../csv/pima-indians-diabetes3.csv', header=0)
 
 # 세부 정보를 X로 지정합니다.
-X = df.iloc[:,0:8]
+X = df.iloc[:, 0:8]
 # 당뇨병 여부를 Y로 지정합니다.
-y = df.iloc[:,8]
+y = df.iloc[:, 8]
 
-params = {
-	'kernel': ['linear', 'poly', 'rbf', 'sigmoid']
-}
-'''
-{'linear', 'poly', 'rbf', 'sigmoid', 'precomputed'}
-'''
+al = XGBClassifier()
+al.fit(X, y)
 
-gs = GridSearchCV(SVR(), params, n_jobs=-1)
-gs.fit(X, y)
-print(gs.best_params_)
-print(gs.best_score_)
-
-cscore = cross_val_score(gs, X, y, cv=5)  # 교차 검증 k=5
-print('accuracy: ',cscore.mean())
-
-
-
+c_score = cross_val_score(al, X, y, cv=5)
+print(f'accuracy: {c_score.mean()}')
+print(X.shape)
