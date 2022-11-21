6a0f56cc17018506cdd0a4dbc174cec77b478db1
minari-c <h990727@gmail.com>
2022-11-21 11:30:59 +0900

    to give a alias matplotlib that mpl

--- week13/code/set_font.py
+++ week13/code/set_font.py
@@ -1,5 +1,5 @@
 import os
-import matplotlib
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
@@ -9,4 +9,6 @@ font_full_path = f'{font_dir_path}{font_file_name}'
 
 
 font_prop = fm.FontProperties(fname=font_full_path, size=15)
-matplotlib.rc('font', family=font_prop.get_name())
+mpl.rc('font', family=font_prop.get_name())
+
+
