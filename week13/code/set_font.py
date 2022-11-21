531ef5ca1d587ee136ab9a817693d9d4bfb883f8
minari-c <h990727@gmail.com>
2022-11-21 11:36:16 +0900

    to solve minus symbol garbled issue

--- week13/code/set_font.py
+++ week13/code/set_font.py
@@ -7,6 +7,8 @@ font_file_name = 'd2coding.ttc'
 font_dir_path = f'{os.path.abspath("../font")}\\'
 font_full_path = f'{font_dir_path}{font_file_name}'
 
+# 마이너스 표시 문제 해결
+mpl.rcParams['axes.unicode_minus'] = False
 
 font_prop = fm.FontProperties(fname=font_full_path, size=15)
 mpl.rc('font', family=font_prop.get_name())
