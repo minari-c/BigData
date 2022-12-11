88aa523ef3f73738805cb5aad12f86e34502e67e
minari-c <h990727@gmail.com>
2022-12-11 18:36:23 +0900

    read xlsx file names

--- final_project/code/test.py
+++ final_project/code/test.py
@@ -6,24 +6,24 @@ import os
 from multiprocessing import Pool, TimeoutError, freeze_support
 
 
-def foo(file_name):
-    print(file_name)
+def foo(file_info):
+    print(file_info)
 
 
 folder_path = '../xlsx/'
+xlsx_path_infos = list()
 
-xlsx_paths = list()
 
 file_names = os.listdir(folder_path)
 for year in file_names:
     # print(os.listdir(f'{folder_path}{year}'))
     for file_name in os.listdir(f'{folder_path}{year}'):
-        xlsx_paths.append(file_name)
+        xlsx_path_infos.append([f'{folder_path}{year}/', file_name])
 
 
-if __name__ == '__main__':
-    freeze_support()
+if __name__ == '__main__':  # for windows os
+    freeze_support()        # for windows os
     with Pool(processes=16) as pool:
-        pool.map(foo, xlsx_paths)
+        pool.map(foo, xlsx_path_infos)
         pool.close()
         pool.join()
