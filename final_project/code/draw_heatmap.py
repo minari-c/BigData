ff93bee2e062c222bf7474a6e5b6866682f83067
minari-c <h990727@gmail.com>
2022-12-12 09:07:59 +0900

    try to multiprocessing in read data

--- final_project/code/draw_heatmap.py
+++ final_project/code/draw_heatmap.py
@@ -1,4 +1,5 @@
-﻿import pandas as pd
+﻿import pandas
+import pandas as pd
 import numpy as np
 from multiprocessing import Pool, freeze_support
 import os
@@ -6,11 +7,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.font_manager as fm
 
+def processing_chunk(chunk):
+    for row in chunk.iterrows():
+        pass
+    time.sleep(5)
+
+
+def foo(corr_df):
+	df_compt = pd.read_csv('../csv/data_compact.csv', low_memory=False)
+	corr_df = df_compt.corr(method='kendall')
+
+
 font_path = '../../font/d2coding.ttc'
 fontprop = fm.FontProperties(fname=font_path, size=15)
 plt.rc('font', family=fontprop.get_name())
 
-df_compt = pd.read_csv('../csv/data_compact.csv', low_memory=False)
+
 # df_all = pd.DataFrame()
 # csv_path_infos = list()
 # for dir_name in os.listdir('../csv'):
@@ -28,12 +40,25 @@ df_compt = pd.read_csv('../csv/data_compact.csv', low_memory=False)
 # for colum in df_all.columns[1:]:
 # 	unique_list.append(df_all[colum].unique())
 # corr_df = df_compt.corr(method='pearson')
-corr_df = df_compt.corr(method='kendall')
-# corr_df = df_compt[1:].corr(method='spearman')
-# corr_df = df_all[['측정소코드', '측정일시', 'SO2', 'CO', 'O3', 'NO2', 'PM10', 'PM25']].corr(method='pearson')
-# corr_df = df[['측정소코드', '측정일시', 'SO2', 'CO', 'O3', 'NO2', 'PM10', 'PM25']].corr(method='kendall')
-# corr_df = df[['측정소코드', '측정일시', 'SO2', 'CO', 'O3', 'NO2', 'PM10', 'PM25']].corr(method='spearman')
-# print(df[['측정일시', 'SO2', 'CO', 'O3', 'NO2', 'PM10', 'PM25']])
+corr_df = pandas.DataFrame()
+if __name__ == '__main__':  # for windows os
+	freeze_support()        # for windows os
+	with Pool(processes=15) as pool:
+		pool_size = 15
+		chunk_size = 2000 * pool_size
+		count = 0
+		for file_chunk in pd.read_csv('../csv/data_compact.csv', low_memory=False, chunksize=chunk_size):
+			line = count * chunk_size
+			print(f"Processing {chunk_size} lines after line {line}")
+			
+			# Split chunk evenly. It's better to use this method if every chunk takes similar time.
+			pool.map(processing_chunk, pd.np.array_split(file_chunk, pool_size))
+			
+			count += 1
+		pool.close()
+		pool.join()
+
+
 print(corr_df)
 mask = np.triu(np.ones(corr_df.shape)).astype(np.bool)
 print(mask)
@@ -52,5 +77,12 @@ sns.heatmap(
 ax.set_title('kendall corr')
 
 plt.show()
+
+# corr_df = df_compt[1:].corr(method='spearman')
+# corr_df = df_all[['측정소코드', '측정일시', 'SO2', 'CO', 'O3', 'NO2', 'PM10', 'PM25']].corr(method='pearson')
+# corr_df = df[['측정소코드', '측정일시', 'SO2', 'CO', 'O3', 'NO2', 'PM10', 'PM25']].corr(method='kendall')
+# corr_df = df[['측정소코드', '측정일시', 'SO2', 'CO', 'O3', 'NO2', 'PM10', 'PM25']].corr(method='spearman')
+# print(df[['측정일시', 'SO2', 'CO', 'O3', 'NO2', 'PM10', 'PM25']])
+
 
 # df_compt.to_csv('../csv/data_concat.csv')
